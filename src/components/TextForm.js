a6ed6ae7100e006c0a2c89323337925e1c04f64e
Mahammad Rahel <rahelmahammad97@gmail.com>
2025-09-07 22:12:56 +0530

    worked on copy button

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -20,7 +20,7 @@ export default function TextForm(props) {
   const handleCopy = () => {
     var text=document.getElementById("myBox");
     text.select();
-    navigator.clipboard.writeText(text);
+    navigator.clipboard.writeText(text.value);
     props.showAlert("copied","Success:");
     
   };
